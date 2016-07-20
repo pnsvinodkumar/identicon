33178662f0bb3702af7a213f477a7834e2f19906
pnsvinodkumar <nagapns@gmail.com>
2016-07-20 12:47:21 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -10,6 +10,10 @@ elif [ "$ENV" = 'UNIT' ]; then
     exec python "tests.py"
 else
     echo "Running Production Server"
-    source /unset_proxies
+    export http_proxy=
+    export https_proxy=
+    export ftp_proxy=
+    export socks_proxy=
+    export no_proxy=
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identicon.py --callable app --stats 0.0.0.0:9191
 fi
